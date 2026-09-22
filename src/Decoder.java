c6440a67c5c978562f841dc55c255cd221fad684
seanroulet <seanroulet@gmail.com>
2026-09-22 15:48:52 +0300

    Initial Commit
    First Commit for the Puzzle of Decoding

---
+++ src/Decoder.java
@@ -0,0 +1,19 @@
+public class Decoder {
+    public static void main(String[] args) {
+        String[] encodedData = {
+                "242837435263orangedqhzmvHedybrightyfjLamarrtableobomco-inventedcherrytlrft",
+                "262737546571oceanforestorzavalleycofnfrequency-hoppinghousenmiudmspreadpapervfpb",
+                "273550607278laptopforestmlaspectrumoceandarkrfrzijtechnologypencilsawimpduringscreenuezw",
+                "263142455456screenpaperojrWorldvalleyjllpiWarlightasjtIIswimmingancientzuoj",
+                "283246506871brightlaptophjlpthatswimmingxbwbvolaidhouseancientofbbtdthescreenlaptoplezb",
+                "233144546568runningxrpzcriticalbrightdarklpofoundationmelonavmeejforrunningpms",
+                "243045536272orangebhfmltmodernchairlightebnitwirelessheavylrfbcomputing,swimmingchairlopu",
+                "293854607080jumpingcherrysesdincludinggrapechairjkgrejWi-Fi,brightncqtBluetooth,flyinglaptopnpl",
+                "273040445859walkinglightyapandlightmbnymGPS.quicklightezubawalkingrvmniu"
+        };
+
+        // TODO: Iterate over encodedData, parse the 3 pairs of indices per element,
+        // and extract/print the decoded message!
+
+    }
+}
